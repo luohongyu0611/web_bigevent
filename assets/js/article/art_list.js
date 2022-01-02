1bc52dab7e6cf04cea332ac8ba53f8c9a0cb3f2e
lhy <llhhyy1999611@163.com>
2022-01-02 17:36:19 +0800

    添加了文章页面

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,5 +1,7 @@
 $(function() {
-    console.log("进入了文章列表页面")
+    var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage
         // data是查询的参数对象，也是请求参数对象，调用接口的时候用data来发起请求
     var q = {
         pagenum: 1,
@@ -7,11 +9,8 @@ $(function() {
         cate_id: '',
         state: ''
     }
-    var layer = layui.layer
-    var form = layui.form
-    var laypage = layui.laypage
-    let render = layui.render
-        // 定义美化时间的过滤器
+
+    // 定义美化时间的过滤器
     template.defaults.imports.dataFormat = function(date) {
         // 根据传入日期来获取 年月日时分秒格式
         const dt = new Date(date)
@@ -38,16 +37,16 @@ $(function() {
             url: '/my/article/list',
             data: q,
             success: function(res) {
-                console.log("获取文章列表数据的接口响应数据")
+                if (res.status !== 0) {
+                    return layer.msg("获取文章列表失败了")
+                }
+                console.log("获取文章列表成功了")
                 console.log(res)
-
-                // 请求成功之后，使用模板引擎渲染页面的数据
-                var htmlStr = template('tpl-table', res)
-                $('body').html(htmlStr)
-                renderPage(res.total) //当文章列表加载完成之后，调用渲染分页的方法
+                    // 使用模板引擎渲染页面的数据
+                let htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                renderPage(res.total)
             }
-
-
         })
     }
 
@@ -71,7 +70,7 @@ $(function() {
     }
 
     // 筛选区域的提交
-    $('#cate_search').on('submit', function(e) {
+    $('#form-search').on('submit', function(e) {
         e.preventDefault()
         let cate_id = $('[name=cate_id]').val() //获取表单中选中项的值
         let state = $('[name=state]').val()
@@ -85,7 +84,7 @@ $(function() {
         console.log("数据总条数")
         console.log(total)
         laypage.render({
-            elem: 'renderPage',
+            elem: 'pageBox',
             count: total,
             limit: q.pagesize,
             curr: q.pagenum,
@@ -105,26 +104,50 @@ $(function() {
     }
 
     $('tbody').on('click', '.btn-delete', function() {
-        console.log("出发了点击事件")
+        console.log("触发了点击事件")
             // 通过代理的形式 出发点击事件，根据id号来删除某行数据
         let id = $(this).attr('data-id')
-        let len = $('.sbtn-delete').length
+        let len = $('.btn-delete').length
         layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
             $.ajax({
-                url: '/my/article/deletecate/' + id,
+                url: '/my/article/delete/' + id,
                 method: 'GET',
                 success: function(res) {
                     if (res.status !== 0) {
                         return layer.msg("删除文章分类失败了")
                     }
-                    return layer.msg("删除文章分类成功了")
+                    layer.msg("删除文章分类成功了")
                     if (len === 1) {
                         q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+
                     }
-                    initTable()
+                    initTable() //根据最新的筛选条件，重新渲染表格数据
                 }
             })
             layer.close(index);
         });
+    })
+
+
+    // 编辑文章
+    $('tbody').on("click", '.btn-edit', function() {
+        console.log("您即将编辑文章") //点击了编辑的按钮之后，弹出一个编辑文章的弹出层
+            // location.href = '/article/art_edit.html'
+            // 需要将以前的数据填充过来
+        let id = $(this).attr('data-id')
+            //    发起请求获取数据
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/' + id,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg("根据id获取文章详情失败了")
+                }
+                // layer.msg("根据id获取文章详情成功了")
+                console.log(res)
+                form.val('form-edit', res.data)
+            }
+        })
+
     })
 })
